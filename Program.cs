a47806c29a7c1b5dbeda68a8e2aec5e0c52bd364
dilshodbekkhamroev98@gmail.com <dilshodbekkhamroev98@gmail.com>
2023-07-18 13:42:12 +0500

    Add Calculation Logic

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System ;
+namespace Calculator{
+
+    class Program{
+        static void Main()
+        { 
+            const float pi = 3.14f;
+            System.Console.WriteLine("Enter values ");
+            System.Console.WriteLine("First number ");
+            string firstUserValue = Console.ReadLine();
+            System.Console.WriteLine("Second number ");
+            string secondUserValue = Console.ReadLine();
+  
+            System.Console.WriteLine("Converting values in progress...");
+            decimal firstNumber = Convert.ToDecimal(firstUserValue);
+            decimal secondNumber = Convert.ToDecimal(secondUserValue);
+              
+            System.Console.WriteLine("Here are your results:");
+            System.Console.WriteLine($"{firstNumber}+{secondNumber}={firstNumber+secondNumber}");
+            System.Console.WriteLine($"{firstNumber}-{secondNumber}={firstNumber-secondNumber}");
+            System.Console.WriteLine($"{firstNumber}*{secondNumber}={firstNumber*secondNumber}");
+            System.Console.WriteLine($"{firstNumber}/{secondNumber}={firstNumber/secondNumber}");
+            System.Console.WriteLine($"{firstNumber}%{secondNumber}={firstNumber%secondNumber}");  
+    }
+}
+}
